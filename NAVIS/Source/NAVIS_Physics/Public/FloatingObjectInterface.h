831812a6a14e8c13a52415299db6360a10862e66
PERARD-G N <perard.git@outlook.com>
2019-10-22 18:28:41 +0000

    added floatingObjectInterface to ease the use of floating forces

---
+++ NAVIS/Source/NAVIS_Physics/Public/FloatingObjectInterface.h
@@ -0,0 +1,36 @@
+// Noe Perard-Gayot <noe.perard@gmail.com> 2019 - All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FloatingObjectInterface.generated.h"
+
+
+UINTERFACE()
+class NAVIS_PHYSICS_API UFloatingObjectInterface : public UInterface
+{
+	//GENERATED_UINTERFACE_BODY()
+    GENERATED_BODY()
+};
+
+class NAVIS_PHYSICS_API IFloatingObjectInterface
+{
+	//GENERATED_IINTERFACE_BODY()
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 	ApplyArchimedesForce()			Calculate Force applied to a component when put in water
+	 *	@param liquidPlane  			Plane of water in world space
+	 * 	@param density				    The density of the liquid
+     *  @param boneName                 Name of the bone to wich apply force, defaults to root
+	 *	@note                           We don't use a FNavisPlane to avoid unnecessary import
+	 *                                  from other modules even thought we added FNavisPlane to the
+	 */
+	UFUNCTION()
+	virtual void ApplyArchimedesForce(const FPlane &liquidPlane, float density = 1.f, FName boneName = NAME_None);
+
+	UFUNCTION()
+	virtual UPrimitiveComponent* GetFloatingComponent() const;
+
+};
